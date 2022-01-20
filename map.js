e8e7c0b86dd02669e95eb62c6cac5164e4f6b8da
Niko Wahlroos <niko.wahlroos@edu.turkuamk.fi>
2022-01-20 23:38:09 +0200

    Removed the dog icon credits (got new ones)

--- map.js
+++ map.js
@@ -12,30 +12,20 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
 var marker = L.marker([60.4518, -22.2666]).addTo(map)
     .bindPopup('<b>Hello world!</b><br />This might be a dog park at some point').openPopup();
 
-//var circle = L.circle([51.508, -0.11], {
-//    color: 'red',
-//    fillColor: '#f03',
-//    fillOpacity: 0.5,
-//    radius: 500
-//}).addTo(map).bindPopup('I am a circle.');
-
-//var polygon = L.polygon([
-//    [51.509, -0.08],
-//    [51.503, -0.06],
-//    [51.51, -0.047]
-//]).addTo(map).bindPopup('I am a polygon.');
-
-
 var popup = L.popup()
     .setLatLng([60.4518, -22.2666])
     .setContent('I am a standalone popup.')
     .openOn(map);
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent('You clicked the map at ' + e.latlng.toString())
-        .openOn(map);
-}
+var DogIcon = L.icon({
+    iconUrl: 'doggie.png',
+    shadowUrl: 'doggie.png',
+    
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
-map.on('click', onMapClick);
+var DogIcon = new DogIcon({iconUrl: 'doggie.png'})
